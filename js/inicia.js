54b06dde57fc84a9c22cca006efdb56a8ed865c6
alegarci503 <alejandro.portillo8@gmail.com>
2017-02-05 20:39:44 -0600

    Se crearon 2 paginas, el Navbar tiene el ultimo diseño

--- js/inicia.js
+++ js/inicia.js
@@ -1,6 +1,24 @@
 $( document ).ready(function(){
      $(".button-collapse").sideNav();
+});
+
+ $(document).ready(function(){
       $('.slider').slider();
+<<<<<<< HEAD
       $('.dropdown-button').dropdown();
       $('.parallax').parallax();
 })
+=======
+    });
+        
+    $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+   $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+      $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+>>>>>>> f1559f87f41b0938a91743ec4fe3b828cc9dd8c3
